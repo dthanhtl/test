6712a730c0aa0d883b61793ea81cfecbab914f2e
dthanhtl <dthanhtl@gmail.com>
2016-01-22 19:34:33 +0700

    init project

---
+++ SSSTest/SSSTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SSSTest
+//
+//  Created by Thanh Tran on 1/22/16.
+//  Copyright © 2016 PizzaThieves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
